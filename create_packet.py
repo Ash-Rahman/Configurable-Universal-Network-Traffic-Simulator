58bb9b52fd879d090faf8b3e46ef156068e3a5d2
Ashikur Rahman <arahman@blandford.local>
2019-09-13 16:58:51 +0100

    Broken IPV6 protocol but added pcap name and traffic transmit source

--- create_packet.py
+++ create_packet.py
@@ -14,105 +14,121 @@ ether_2_addr = "90:e2:ba:aa:69:05"
 complete_packet_list = []
 
 """
-Purpose: Clears the array of packets once the generated flows have been saved to pcap
-parameters: N/A
-Returns: N/A
-"""
-def clear_complete_packet_list():
-    global complete_packet_list
-    complete_packet_list = []
-
-"""
 Purpose: Generates a tcp flow
 parameters: number_of_flows (int) takes a number of flows to generate
 Returns: N/A
 """
-def create_tcp_flow(number_of_flows):
+def create_tcp_flow(number_of_flows, ipv_type):
     packet_list = []
     pktSeq1 = 1
     pktSeq2 = 1
     count = 0
 
+    ipv_x = int(ipv_type)
     while (count < int(number_of_flows)):
-        ipv4Add1 = generate_ipv4_addr()
-        ipv4Add2 = generate_ipv4_addr()
+        if ipv_x == 4:
+            ipv_x_addr1 = generate_ipv4_addr()
+            ipv_x_addr2 = generate_ipv4_addr()
+        elif ipv_x == 6:
+            ipv_x_addr1 = generate_ipv6_addr()
+            ipv_x_addr2 = generate_ipv6_addr()
+        else:
+            ipv_x_addr1 = generate_ipv4_addr()
+            ipv_x_addr2 = generate_ipv4_addr()
 
         # SYN message
-        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv4Add1,dst=ipv4Add2)/TCP(sport=26,dport=40,flags='S',seq=0,ack=0)
+        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv_x_addr1,dst=ipv_x_addr2)/TCP(sport=26,dport=40,flags='S',seq=0,ack=0)
         packet_list.extend(p)
 
         # SYN / ACK
-        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv4Add2,dst=ipv4Add1)/TCP(sport=40,dport=26,flags='SA',seq=0, ack=1)
+        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv_x_addr2,dst=ipv_x_addr1)/TCP(sport=40,dport=26,flags='SA',seq=0, ack=1)
         packet_list.extend(p)
 
         # ACK message
-        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv4Add1,dst=ipv4Add2)/TCP(sport=26,dport=40,flags='A',seq=1, ack=1)
+        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv_x_addr1,dst=ipv_x_addr2)/TCP(sport=26,dport=40,flags='A',seq=1, ack=1)
         packet_list.extend(p)
 
         # PSH / ACK message
-        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv4Add1,dst=ipv4Add2)/TCP(sport=26,dport=40,flags='PA',seq=pktSeq1)/Raw(RandString(size=1))
+        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv_x_addr1,dst=ipv_x_addr2)/TCP(sport=26,dport=40,flags='PA',seq=pktSeq1)/Raw(RandString(size=1))
         packet_list.extend(p)
         pktSeq1 += count;
 
         # PSH / ACK message
-        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv4Add2,dst=ipv4Add1)/TCP(sport=40,dport=26,flags='PA',seq=pktSeq2)/Raw(RandString(size=1))
+        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv_x_addr2,dst=ipv_x_addr1)/TCP(sport=40,dport=26,flags='PA',seq=pktSeq2)/Raw(RandString(size=1))
         packet_list.extend(p)
         pktSeq2 += count;
 
         # FIN message
-        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv4Add1,dst=ipv4Add2)/TCP(sport=26,dport=40,flags='F',seq=pktSeq1,ack=0)
+        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv_x_addr1,dst=ipv_x_addr2)/TCP(sport=26,dport=40,flags='F',seq=pktSeq1,ack=0)
         packet_list.extend(p)
 
         # FIN / ACK
-        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv4Add2,dst=ipv4Add1)/TCP(sport=40,dport=26,flags='FA',seq=pktSeq2, ack=1)
+        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv_x_addr2,dst=ipv_x_addr1)/TCP(sport=40,dport=26,flags='FA',seq=pktSeq2, ack=1)
         packet_list.extend(p)
 
         count += 1
 
     complete_packet_list.extend(packet_list)
     packet_list = []
+    print("\nTCP flow created, remember to save the pcap!")
 
 """
 Purpose: Generates a udp flow
 parameters: number_of_flows (int) takes a number of flows to generate
 Returns: N/A
 """
-def create_udp_flow(number_of_flows):
+def create_udp_flow(number_of_flows, ipv_type):
     packet_list = []
     count = 0
 
+    ipv_x = int(ipv_type)
     while (count < int(number_of_flows)):
-        ipv4Add1 = generate_ipv4_addr()
-        ipv4Add2 = generate_ipv4_addr()
+        if ipv_x == 4:
+            ipv_x_addr1 = generate_ipv4_addr()
+            ipv_x_addr2 = generate_ipv4_addr()
+        elif ipv_x == 6:
+            ipv_x_addr1 = generate_ipv6_addr()
+            ipv_x_addr2 = generate_ipv6_addr()
+        else:
+            ipv_x_addr1 = generate_ipv4_addr()
+            ipv_x_addr2 = generate_ipv4_addr()
 
-        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv4Add1,dst=ipv4Add2)/UDP(sport=26,dport=40)/Raw(RandString(size=1))
+        p=Ether(src=ether_1_addr, dst=ether_2_addr)/IP(src=ipv_x_addr1,dst=ipv_x_addr2)/UDP(sport=26,dport=40)/Raw(RandString(size=1))
         packet_list.extend(p)
 
-        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv4Add2,dst=ipv4Add1)/UDP(sport=40,dport=26)/Raw(RandString(size=1))
+        p=Ether(src=ether_2_addr, dst=ether_1_addr)/IP(src=ipv_x_addr2,dst=ipv_x_addr1)/UDP(sport=40,dport=26)/Raw(RandString(size=1))
         packet_list.extend(p)
 
         count += 1
 
     complete_packet_list.extend(packet_list)
     packet_list = []
+    print("\nUDP flow created, remember to save the pcap!")
 
 """
 Purpose: Transmits pcap file through interface
 parameters: interface (string) takes a network interface name
 Returns: N/A
 """
-def play_pcap(interface):
+def transmit_traffic(interface, traffic_source, optional_pcap_name=""):
     try:
-        print("\nPlaying pcap")
-        print("\nThis may take some time")
+        if int(traffic_source) == 1:
+            pcap = rdpcap(optional_pcap_name)
+            print("\nPlaying traffic from " + optional_pcap_name)
+        else:
+            pcap = complete_packet_list
+            print("\nPlaying traffic from application memory")
+
         s = conf.L3socket(iface=interface)
-        pcap = rdpcap("user_generated.pcap")
+        print("\nPlaying traffic, this may take some time")
         for pkt in pcap:
             s.send(pkt)
+    except IOError as IOerror:
+        print("\nERROR: " + str(IOerror))
+    except Exception as e:
+        print("\nUnexpected ERROR: " + str(e))
+    else:
         print("\nDone!")
-    except IOError as error:
-        print("\nPlease select a valid interface")
-        print("\n" + str(error))
 
 """
 Purpose: Generate a random ipv4 address
@@ -127,15 +143,36 @@ def generate_ipv4_addr():
     return addr_str
 
 """
+Purpose: Generate a random ipv6 address
+parameters: N/A
+Returns: (string) Ipv6 address
+"""
+def generate_ipv6_addr():
+    bits = getrandbits(128)
+    addr = IPv6Address(bits)
+    addr_str = str(addr.exploded)
+
+    return addr_str
+
+"""
 Purpose: Take array of flows and write to pcap.
 parameters: N/A
 Returns: N/A
 """
-def create_pcap_file():
+def create_pcap_file(filename):
     if len(complete_packet_list) != 0:
         print ("\nCreating your Pcap, this may take some time...")
-        wrpcap("user_generated.pcap", complete_packet_list)
+        wrpcap(filename, complete_packet_list)
         clear_complete_packet_list()
-        print ("\nPcap 'user_generated.pcap' created!")
+        print ("\nFile " + filename + " created!")
     else:
         print ("\nYou have not created any packets to save!")
+
+"""
+Purpose: Clears the array of packets once the generated flows have been saved to pcap
+parameters: N/A
+Returns: N/A
+"""
+def clear_complete_packet_list():
+    global complete_packet_list
+    complete_packet_list = []
--- traffic_simulator.py
+++ traffic_simulator.py
@@ -5,6 +5,7 @@ import sys, os
 import create_packet
 import numbers
 import time
+import string
 
 
 header = "\
@@ -14,72 +15,90 @@ ___________              _____  _____.__           ________                     
   |    |   |  | \// __ \|  |   |  |  |  \  \___  \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  | (  <_> )  | \/ \n\
   |____|   |__|  (____  /__|   |__|  |__|\___  >  \______  /\___  >___|  /\___  >__|  (____  /__|  \____/|__|   \n\
                       \/                     \/          \/     \/     \/     \/           \/                   \n"
-def create_pcap_menu():
-    print ("\nYou selected: Create pcap!")
+def create_traffic_menu():
+    print ("\nYou selected: Create traffic!")
     choice1 = raw_input("""  
                 A: Create TCP flow
                 B: Create UDP flow
                 C: Save PCAP
                 D: Return to main menu
                 Please enter your choice: """)
+
     if choice1 == "A" or choice1 =="a":
         number_of_TCP_flows = raw_input("\nEnter number of TCP flows: ")
-        if number_of_TCP_flows.isdigit():
-            create_packet.create_tcp_flow(number_of_TCP_flows)
-            print ("\nTCP flow created, remember to save the pcap!")
-            create_pcap_menu()
+        ipv_type = raw_input("\nEnter IPV type (4/6): ")
+        if number_of_TCP_flows.isdigit() and ipv_type.isdigit():
+            create_packet.create_tcp_flow(number_of_TCP_flows, ipv_type)
+            create_traffic_menu()
         else:
-            print ("\nPlease Enter a valid number!")
-            create_pcap_menu()
+            print ("\nPlease Enter a valid number of flows and IPV(4/6) type!")
+            create_traffic_menu()
+
     elif choice1 == "B" or choice1 =="b":
         number_of_UDP_flows = raw_input("\nEnter number of UDP flows: ")
-        if number_of_UDP_flows.isdigit():
-            create_packet.create_udp_flow(number_of_UDP_flows)
-            print ("\nUDP flow created, remember to save the pcap!")
-            create_pcap_menu()
+        ipv_type = raw_input("\nEnter IPV type (4/6): ")
+        if number_of_UDP_flows.isdigit() and ipv_type.isdigit():
+            create_packet.create_udp_flow(number_of_UDP_flows, ipv_type)
+            create_traffic_menu()
         else:
-            print ("\nPlease Enter a valid number!")
-            create_pcap_menu()
+            print ("\nPlease Enter a valid number of flows and IPV(4/6) type!")
+            create_traffic_menu()
+
     elif choice1 == "C" or choice1 =="c":
-        create_packet.create_pcap_file()
-        create_pcap_menu()
+        filename = raw_input("\nEnter filename (e.g tcp_flow.pcap): ")
+        if not filename and not filename.strip():
+            print("\nEnter valid filename (Only Letters and numbers followed by .pcap)")
+        else:
+            create_packet.create_pcap_file(filename)
+        create_traffic_menu()
+
     elif choice1 == "D" or choice1 =="d":
         os.system('clear')
         display_menu_screen()
+
     else:
         print("\nYou must only select either A, B or C")
         print("\nPlease try again")
-    create_pcap_menu()
+    create_traffic_menu()
 
-def play_pcap():
+def play_traffic():
     print ("\nYou selected: Play pcap! ")
     print ("\nHere is a list of available interfaces!\n ")
     os.system('ifconfig')
+    print ("\nYou can play traffic from either a pcap or what was created in the create traffic menu.\n ")
     interface = raw_input("\nEnter interface to play pcap out of: ")
-    create_packet.play_pcap(interface)
+    traffic_source = raw_input("\nEnter [1] to play traffic from pcap \nEnter [2] to play traffic from application")
+
+    if int(traffic_source) == 1:
+        print ("\nList of available pcaps: \n")
+        os.system('ls pcaps/')
+        pcap = raw_input("\nEnter name of pcap to transmit: ")
+        create_packet.transmit_traffic(interface, traffic_source, pcap)
+    else:
+        create_packet.transmit_traffic(interface, traffic_source)
     display_menu_screen()
 
-
 def display_menu_screen():
-    print (header)
-    print "version 0.1\n"
+    print(header)
+    print("version 0.1\n")
 
     choice = raw_input("""
-                A: Create Pcap
-                B: Play Pcap
+                A: Create Traffic
+                B: Play Traffic
                 Q: Quit
 
                 Please enter your choice: """)
 
-    if choice == "A" or choice =="a":
-        create_pcap_menu()
-    elif choice == "B" or choice =="b":
-        play_pcap()
-    elif choice=="Q" or choice=="q":
+    if choice == "A" or choice == "a":
+        create_traffic_menu()
+    elif choice == "B" or choice == "b":
+        play_traffic()
+    elif choice== "Q" or choice== "q":
         sys.exit()
     else:
         print("You must only select either A or B")
         print("Please try again")
+        display_menu_screen()
 
 def main():
     os.system('clear')
